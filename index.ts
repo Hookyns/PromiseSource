bd7aad9d7ee6c2fa69af51312b20e62acb32d9a4
HookCZ <ncm.Hawkyns@seznam.cz>
2020-06-11 21:06:57 +0200

    Source commited

---
+++ index.ts
@@ -0,0 +1,149 @@
+﻿/**
+ * Promise manageable from outer scope
+ */
+export default class PromiseSource<TResult>
+{
+	/**
+	 * Promise
+	 * @internal
+	 */
+	private readonly _promise: Promise<TResult>;
+
+	/**
+	 * Resolve callback
+	 * @internal
+	 */
+	private _resolve: (value?: (PromiseLike<TResult> | TResult)) => void;
+
+	/**
+	 * Reject callback
+	 * @internal
+	 */
+	private _reject: (reason?: any) => void;
+
+	/**
+	 * Set after resolve
+	 * @internal
+	 */
+	private _resolved: boolean;
+
+	/**
+	 * Set after reject
+	 * @internal
+	 */
+	private _rejected: boolean;
+
+	/**
+	 * Timeout identifier
+	 * @internal
+	 */
+	private _timeout?: number;
+
+	/**
+	 * Get managed Promise
+	 */
+	public get promise(): Promise<TResult>
+	{
+		return this._promise;
+	}
+
+	/**
+	 * True when managed promise was resolved
+	 */
+	public get resolved(): boolean
+	{
+		return this._resolved;
+	}
+
+	/**
+	 * True when managed promise was rejected
+	 */
+	public get rejected(): boolean
+	{
+		return this._rejected;
+	}
+
+	/**
+	 * True when resolved or rejected
+	 */
+	public get completed()
+	{
+		return this._rejected || this._resolved;
+	}
+
+	/**
+	 * Ctor
+	 * @param {number} [timeout] Optional timeout
+	 */
+	constructor(timeout?: number)
+	{
+		this._promise = new Promise((resolve, reject) => {
+			this._resolve = resolve;
+			this._reject = reject;
+		})
+
+		if (timeout != undefined)
+		{
+			this._timeout = setTimeout(() => {
+				if (!this.completed)
+				{
+					this.reject(new Error("Promise timed out"));
+				}
+			}, ~~timeout);
+		}
+	}
+
+	/**
+	 * Reject managed promise
+	 * @param reason
+	 */
+	public reject(reason?: any)
+	{
+		this.validateCompletion();
+
+		this._reject(reason);
+		this._rejected = true;
+
+		this.dispose();
+	}
+
+	/**
+	 * Resolve managed promise
+	 * @param value
+	 */
+	public resolve(value?: TResult)
+	{
+		this._resolve(value);
+		this._resolved = true;
+
+		this.dispose();
+	}
+
+	/**
+	 * Clear handlers
+	 * @internal
+	 */
+	private dispose()
+	{
+		this._resolve = undefined;
+		this._reject = undefined;
+
+		if (this._timeout != undefined)
+		{
+			clearTimeout(this._timeout);
+			this._timeout = undefined;
+		}
+	}
+
+	/**
+	 * Validate completion
+	 * @internal
+	 */
+	private validateCompletion()
+	{
+		if (this.completed)
+		{
+			throw Error("PromiseSource was already " + (this._rejected ? "rejected" : "resolved") + ".");
+		}
+	}
+}
---
+++ test.js
@@ -0,0 +1,89 @@
+﻿const PromiseSource = require("./index").default;
+
+try {
+    // Test resolve
+    // --------------------------------------------------------------------------------------------------------------
+    let p1 = new PromiseSource();
+
+    p1.promise.then(val => {
+        if (val !== 999) {
+            throw new Error("Failed: Resolved value is correct");
+        }
+    })
+
+    p1.resolve(999);
+
+    setTimeout(() => {
+        if (!p1.resolved || !p2.completed) {
+            throw new Error("Failed: Promise was resolved correctly");
+        }
+    });
+
+    // Test reject
+    // --------------------------------------------------------------------------------------------------------------
+    let p2 = new PromiseSource();
+
+    p2.promise.catch(reason => {
+        if (!(reason instanceof Error) || reason.message !== "failed") {
+            throw new Error("Failed: Reject reason is correct");
+        }
+    })
+
+    p2.reject(new Error("failed"));
+
+    setTimeout(() => {
+        if (!p2.rejected || !p2.completed) {
+            throw new Error("Failed: Promise was rejected correctly");
+        }
+    });
+
+    // Test timeout
+    // --------------------------------------------------------------------------------------------------------------
+    let p3 = new PromiseSource(10);
+
+    p3.promise.catch((reason) => {
+        if (!(reason instanceof Error)) {
+            throw new Error("Failed: Promise rejected with Error after timeout");
+        }
+    })
+    
+    setTimeout(() => {
+        if (!p3.rejected || !p3.completed) {
+            throw new Error("Failed: Promise rejected with Error after timeout");
+        }
+    }, 11);
+
+    // Test repeatedly complete
+    // --------------------------------------------------------------------------------------------------------------
+    let p4 = new PromiseSource();
+    p4.resolve();
+    
+    let catched = false;
+    
+    try {
+        p4.resolve();
+    }
+    catch (e) {
+        catched = true;
+    }
+    
+    if (!catched) {
+        throw new Error("Failed: Repeated completion cause Error");
+    }
+    
+    
+    setTimeout(() => {
+        if (!p2.rejected || !p2.completed) {
+            throw new Error("Failed: Promise was rejected correctly");
+        }
+    });
+}
+catch (e) {
+    console.error(e.message);
+    process.exit(1);
+}
+
+process.on("unhandledRejection", e => {
+    console.error(e.message);
+    process.exit(1);
+});
